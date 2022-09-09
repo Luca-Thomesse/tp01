a3c87cda8e9a2c62a1a0b6613a7fbb54c73411e8
Luca-Thomesse <luca.thomesse@edu.univ-fcomte.fr>
2022-09-09 11:44:47 +0200

    Props et v-bind

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
     import card from "../components/Card.vue";
     import headercard from "../components/Header.vue";
     import footercard from "../components/Footer.vue";
@@ -6,6 +6,41 @@
       name: "App",
       components: { card, headercard, footercard },
     };
+    const maison = {
+          image:"/images-maison/maison3.jpg",
+          titre:"Faulkner Ave",
+          lieu:"909 Woodland St, Michigan, IN",
+          price:4550,
+          nbrSDB:3
+    }
+    const maison1 = {
+          image:"/images-maison/maison6.jpg",
+          titre:"Tarpon Bay",
+          lieu:"103 Lake Shores, Michigan, IN",
+          price:4800,
+          nbrSDB:3
+    }
+    const maison2 = {
+          image:"/images-maison/maison4.jpg",
+          titre:"St. Christal",
+          lieu:"210 US Highway, HighlandLake, FL",
+          price:4650,
+          nbrSDB:2
+    }
+    const maison3 = {
+          image:"/images-maison/maison2.jpg",
+          titre:"Palm Harbor",
+          lieu:"2699 Green Valley, Highland Lake",
+          price:2095,
+          nbrSDB:2
+    }
+    const maison4 = {
+          image:"/images-maison/maison1.jpg",
+          titre:"Beverly Springfield",
+          lieu:"2821 Lake Sevilla, Palm Harbor, TX",
+          price:3145,
+          nbrSDB:2 
+    }
     </script>
     
     <template class="bg-indigo-25">
@@ -55,7 +90,11 @@
           :price="4550"
           nbrSDB="3"
         />
-        <card class="p-8" image="/images-maison/maison5.jpg" titre="Cover Red" lieu="243 CurlewRoad, PalmHarbor, TX" :price="3100" nbrSDB="2" />
+        <card class="p-8" v-bind="maison" />
+        <card class="p-8" v-bind="maison1" />
+        <card class="p-8" v-bind="maison2" />
+        <card class="p-8" v-bind="maison3" />
+        <card class="p-8" v-bind="maison4" />
       </div>
       <footercard />
     </template>
