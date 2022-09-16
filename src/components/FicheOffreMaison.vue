b9bd8ccd2680e9583be8b9f6df67a85aa8b9ecf1
Luca-Thomesse <luca.thomesse@edu.univ-fcomte.fr>
2022-09-16 11:55:07 +0200

    Mise en forme (fin tp)

--- src/components/FicheOffreMaison.vue
+++ src/components/FicheOffreMaison.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import { ref } from "@vue/reactivity";
-import Card from "./card.vue";
+    import Card from "./card.vue";
     // On fait une variable réactive qui référence les données
     // ATTENTION : faire une Ref pas Reactive car :
     // C'est l'objet qui doit être réactif, pas ses props
@@ -16,10 +16,10 @@ import Card from "./card.vue";
         </div>
         <div class="p-2">
             <!-- On passe la "ref" à Formkit -->
-            <FormKit type="form" v-model="maison">
+            <FormKit type="form" v-model="maison" :config="{ classes: { input: 'p-1 rounded border-gray-300 shadow-sm border',label: 'text-gray-600',},}" :submit-attrs="{ classes: { input: 'bg-red-300 p-1 rounded' } }">
                 <FormKit name="nom" label="nom" />
                 <FormKit name="prix" label="prix" type="number" />
-                <FormKit name="favori" label="mettre en valeur" type="checkbox" />
+                <FormKit name="favori" label="mettre en valeur" type="checkbox" wrapper-class="flex"/>
             </FormKit>
         </div>
     </div>
