839829d2d523f1b963d3250c78a4eb1bbca12452
Luca-Thomesse <luca.thomesse@edu.univ-fcomte.fr>
2022-09-30 10:21:58 +0200

    Test supabase console

--- src/pages/liste-supabase.vue
+++ src/pages/liste-supabase.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
-    import Card from "../components/card.vue";
-    const res = await fetch("/maison.json");
-    const listeMaisons = await res.json();
+    import { supabase } from "../supabase";
+    console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+    const maisons = []; // à remplacer par l'appel à Supabase
 
 
 </script>
 
 <template>
-    <div class="p-2">
-        <h2>Page liste fetch maisons</h2>
-        <Card v-bind="listeMaisons[0]"/>
-        <Card v-bind="listeMaisons[1]"/>
-        <div v-for="uneMaison in listeMaisons" :key="uneMaison.nom">
-          <card v-bind="uneMaison" />
-        </div>
-    </div>
+    
 </template>
